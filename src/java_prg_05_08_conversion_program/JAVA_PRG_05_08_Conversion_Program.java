9305cb569585a8b1174098a94153e79e580d5cfe
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 15:33:09 -0500

    Prompt written.

---
+++ src/java_prg_05_08_conversion_program/JAVA_PRG_05_08_Conversion_Program.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_08_conversion_program;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that asks the user to enter a distance in meters. The
+ * program will then present the following menu of selections:
+ * 
+ *      1. Convert to kilometers
+ *      2. Convert to inches
+ *      3. Convert to feet
+ *      4. Quit the program
+ * 
+ * The program will convert the distance to kilometers, inches, or feet
+ * depending on the user's selection. Here are the specific requirements:
+ * 
+ *      * Write a void method named showKilometers, which accepts a number of
+ *      meters as an argument. The method should display the argument
+ *      converted to kilometers. Convert the meters to kilometers using the
+ *      following formula:
+ * 
+ *          kilometers = meters * 0.001
+ * 
+ *      * Write a void method named showInches, which accepts a number of
+ *      meters as an argument. The method should display the argument
+ *      converted to inches. Convert the meters to inches using the
+ *      following formula:
+ * 
+ *          inches = meters * 39.37
+ * 
+ *      * Write a void method named showFeet, which accepts a number of
+ *      meters as an argument. The method should display the argument
+ *      converted to feet. Convert the meters to feet using the
+ *      following formula:
+ *  
+ *          feet = meters * 3.281
+ * 
+ *      * Write a void method named menu that displays a menu of selections.
+ *      This method should not accept any arguments.
+ *      
+ *      * The program should continue to display the menu until the user enters
+ *      4 to quit the program.
+ * 
+ *      * The program should not accept negative numbers for the distance in
+ *      meters.
+ * 
+ *      * If the user selects an invalid choice from the menu, the program
+ *      should display an error message.
+ * 
+ *      Here is an example session with the program, using console input. The
+ *      user's input is shown in bold.
+ * 
+ *          Enter a distance in meters: 500 [Enter]
+ *          1. Convert to kilometers
+ *          2. Convert to inches
+ *          3. Convert to feet
+ *          4. Quit the program
+ * 
+ *          Enter your choice: 1 [Enter]
+ *          500 meters is 0.5 kilometers.
+ * 
+ *          1. Convert to kilometers
+ *          2. Convert to inches
+ *          3. Convert to feet
+ *          4. Quit the program
+ * 
+ *          Enter your choice: 3 [Enter]
+ *          500 meters is 1640.5 feet.
+ * 
+ *          1. Convert to kilometers
+ *          2. Convert to inches
+ *          3. Convert to feet
+ *          4. Quit the program
+ * 
+ *          Enter your choice: 4 [Enter]
+ *          Bye!
+ *          
+ */
+public class JAVA_PRG_05_08_Conversion_Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
